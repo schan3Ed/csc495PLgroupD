29b2ca0604d64aa98b7bba6bf01a9bf816246650
Connor Crothers <cdcrothe@ncsu.edu>
2018-04-05 18:15:46 -0400

    init card class env

--- project/card.py
+++ project/card.py
@@ -2,6 +2,6 @@ import * from base
 class Card(o):
     __init__(self, name, suit, rank, func=None):
         self.func = func or lambda: None
-        self.name = ''
-        self.suit=''
-        self.rank=''
+        self.name = str(name)
+        self.suit = str(suit)
+        self.rank = str(rank)
