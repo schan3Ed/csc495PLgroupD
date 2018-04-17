f5cd101e28206ceff66f7fb0a988b0aaf1fb4742
Edward Chan <edo@Edwards-MacBook-Pro.local>
2018-04-16 23:01:13 -0400

    updated example and redo the folders

--- project/__main__.py
+++
@@ -1,9 +0,0 @@
-# vim: set filetype=python ts=4 sw=2 sts=2 expandtab:
-from init import *
-from base import *
-
-# ---------------------------------------
-if __name__ == "__main__":
-    rseed()
-    compiler.run()
-    # bartok.run()
